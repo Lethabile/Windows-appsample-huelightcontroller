79b599af43df1fd03b4bfbd09c63f74410cfac17
Kobe Taylor <kobe.taylor@gmail.com>
2020-11-28 18:49:23 +1300

    Removed Bluetooth LE and Cortana support

--- HueApp/Initializer.xaml.cs
+++ HueApp/Initializer.xaml.cs
@@ -79,7 +79,7 @@ namespace HueApp
         {
             await FindBridgeAsync();
             await FindLightsAsync();
-            await InitializeCortanaAsync();
+            // await InitializeCortanaAsync();
             SaveBridgeToCache();
             await rootFrame.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
@@ -190,55 +190,6 @@ namespace HueApp
             {
                 await ReportErrorAsync(
                     "We encountered an unexpected problem trying to find your lights: " + e);
-            }
-        }
-
-        /// <summary>
-        /// Prepares Cortana for background use. 
-        /// </summary>
-        private async Task InitializeCortanaAsync()
-        {
-            // You can't write to application files by default, so we need to create a 
-            // secondary VCD file to dynamically write Cortana commands to.
-            StorageFile dynamicFile = await ApplicationData.Current.RoamingFolder.CreateFileAsync(
-                "VoiceCommands.xml", CreationCollisionOption.ReplaceExisting);
-
-            // Load the base file and parse the PhraseList we want from it.
-            StorageFile baseFile = await StorageFile.GetFileFromApplicationUriAsync(
-                new Uri("ms-appx:///VoiceCommands.xml"));
-            XDocument xml = XDocument.Load(baseFile.Path);
-            XElement state = xml.Descendants().First(x => x.Name.LocalName == "PhraseList" &&
-                null != x.Attribute("Label") && x.Attribute("Label").Value == "state");
-
-            // A ColorMapping is a RGB and HSV compliant representation a system color.
-            // ColorMapping.CreateAll() returns a ColorMapping for all system colors available to UWP apps.
-            // For each ColorMapping, add it to the list of phrases Cortana knows.
-            foreach (HsbColor color in HsbColor.CreateAll())
-            {
-                state.Add(new XElement("Item", color.Name));
-            }
-
-            // Add the light names.
-            XElement names = xml.Descendants().First(x => x.Name.LocalName == "PhraseList" &&
-                null != x.Attribute("Label") && x.Attribute("Label").Value == "name");
-            foreach (Light light in _lights)
-            {
-                names.Add(new XElement("Item", light.Name));
-            }
-
-            // Save the file, and then load so Cortana recognizes it.
-            using (Stream stream = await dynamicFile.OpenStreamForWriteAsync())
-            {
-                xml.Save(stream);
-            }
-            try
-            {
-                await VoiceCommandDefinitionManager.InstallCommandDefinitionsFromStorageFileAsync(dynamicFile);
-            }
-            catch (FileNotFoundException)
-            {
-                // Do nothing. This is a workaround for a spurious FileNotFoundException that 
-                // is thrown even though dynamicFile exists on disk. 
             }
         }
 
